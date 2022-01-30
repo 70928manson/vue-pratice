69b778deeafc6b84dd4534c8204c7569a5ef295d
Manson <manson972@yahoo.com>
2022-01-30 23:26:22 +0800

    更新程式碼

--- js/index.js
+++ js/index.js
@@ -1,22 +1,26 @@
 import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.26/vue.esm-browser.min.js';
 
 const url = 'https://vue3-course-api.hexschool.io/v2'; // API平台
-const path = 'manson972'; //個人 API Path
+const path = 'manson972';  //個人 API Path
 
 const app = createApp({
-    data(){
+    data() {
         return {
-            username: '',
-            password: ''
+            user: {
+                username: '',
+                password: ''
+            }
         }
     },
     methods: {
-        login(){
+        login() {
             axios.post(`${url}/admin/signin`, this.user)
                 .then((res) => {
+                    console.log(res);
                     const { token, expired } = res.data;
-                    document.cookie = `hexToken=${token}; expires=${new Date(expired)}`;
-                    window.location = `products.html`;
+                    console.log(token, expired);
+                    document.cookie = `hexToken=${token}; expires=${new Date(expired)};`;
+                    window.location = 'products.html';
                 })
                 .catch((err) => {
                     alert(err.data.message);
@@ -24,7 +28,7 @@ const app = createApp({
         }
     },
     mounted() {
-        ;
+
     }
 });
 
--- js/product.js
+++ js/product.js
@@ -4,7 +4,7 @@ const app = createApp({
     data() {
         return {
             url: 'https://vue3-course-api.hexschool.io/v2',
-            api_path: 'manson972',
+            api_Path: 'skps0102',
             products: [],
             itemTemp: {},
         }
@@ -13,20 +13,33 @@ const app = createApp({
         checkLogin() {
             //確認用戶是否登入成功
             axios.post(`${this.url}/api/user/check`)
-              .then((res) => {
-                  this.getProductsData();
-              })
-              .catch((err) => {
-                  alert(err.data.message);
-                  //登入失敗 導回原本登入畫面
-                  window.location = 'index.html';
-              })
+                .then((res) => {
+                    console.log('resp ', res);
+                    this.getProductsData();
+                })
+                .catch((err) => {
+                    alert(err.data.message);
+                    //登入失敗 導回原本登入畫面
+                    window.location = 'index.html';
+                })
+        },
+        getProductsData() {
+            axios.get(`${this.url}/api/${this.api_Path}/admin/products`)
+                .then((res) => {
+                    const { products } = res.data;
+                    this.products = products;
+                })
+                .catch((err) => {
+                    alert(err.data.message);
+                    console.log(err);
+                })
         }
     },
     created() {
-        //取得Token
+        // 取得 Token
         const token = document.cookie.replace(/(?:(?:^|.*;\s*)hexToken\s*\=\s*([^;]*).*$)|^.*$/, "$1");
-        axios.defaults.headers.common['Authoriazation'] = token;
+        console.log('get Cookie token', token);
+        axios.defaults.headers.common['Authorization'] = token;
 
         this.checkLogin();
     }
